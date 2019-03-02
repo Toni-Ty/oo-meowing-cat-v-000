69ed9e80fcbc2963e512cdd2bc5f7bae42a48054
Toni-Ty <antonique2479@yahoo.com>
2019-03-02 03:37:39 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,11 +1,11 @@
 class Cat
   attr_accessor(:name, :meow)
 
-def initialize(name, meow)
-  @name = name
-  @meow = meow
-end
-end
+# def initialize(name, meow)
+#   @name = name
+#   @meow = meow
+# end
+# end
 
 
 
